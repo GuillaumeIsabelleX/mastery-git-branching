fd00b697f0aa026dd090c5e9fb340a720c06fba9
Guillaume Isabelle <jgi@jgwill.com>
2019-03-24 12:26:01 -0400

    fixed bug

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ var z = 0;
 
 var p = i / z; //lets bug it...
 
-throw new Error('It is rare, but this bug is VOLONTARY to bug the Continous integration');
+//throw new Error('It is rare, but this bug is VOLONTARY to bug the Continous integration');
 
-console.log("that should not print out");
+console.log("Bug is fixed");
 
