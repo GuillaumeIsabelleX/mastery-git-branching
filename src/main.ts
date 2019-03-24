6ea66a4e1f4cae6bceaf970a4adf21c4e27b1a63
Guillaume Isabelle <jgi@jgwill.com>
2019-03-24 12:24:02 -0400

    feature bugging things take 3

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ var i = 1;
 var z = 0;
 
 var p = i / z; //lets bug it...
-throw;
+
+throw new Error('It is rare, but this bug is VOLONTARY to bug the Continous integration');
 
 console.log("that should not print out");
 
